fa1f865f8c678f57d38fa96568c53633d7ecf04a
Milton Mazzarri <milmazz@gmail.com>
2022-02-12 12:59:40 -0600

    replace deprecated Mix.Config

--- turtle_ui/config/config.exs
+++ turtle_ui/config/config.exs
@@ -1,6 +1,6 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 # Configure the main viewport for the Scenic application
 config :turtle_ui, :viewport, %{
